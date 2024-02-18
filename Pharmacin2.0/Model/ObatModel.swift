9c34e7559ef3e9d4d7f941b8d0d390b45a349c17
Devin <devin@cyberindo-sinergi.com>
2024-02-18 11:21:30 +0700

    New Design

---
+++ Pharmacin2.0/Model/ObatModel.swift
@@ -0,0 +1,8 @@
+//
+//  ObatModel.swift
+//  Pharmacin2.0
+//
+//  Created by Cyberindo Sinergi Sistem on 17/02/24.
+//
+
+import Foundation
---
+++ Pharmacin2.0/View/Component/KasirTambahPenjualanComponent/TambahObatPopUp.swift
@@ -0,0 +1,18 @@
+//
+//  TambahObatPopUp.swift
+//  Pharmacin2.0
+//
+//  Created by Cyberindo Sinergi Sistem on 16/02/24.
+//
+
+import SwiftUI
+
+struct TambahObatPopUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TambahObatPopUp()
+}
