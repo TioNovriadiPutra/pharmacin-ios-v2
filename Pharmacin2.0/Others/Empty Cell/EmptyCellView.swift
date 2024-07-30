260f1e7576aa99be214d21bf9839cb9b1aaf2000
Devin <devin.maleke@gmail.com>
2024-07-30 23:19:35 +0700

    Update features

---
+++ Pharmacin2.0/Others/Empty Cell/EmptyCellView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyCellView.swift
+//  Pharmacin2.0
+//
+//  Created by Devin Maleke on 30/07/24.
+//
+
+import SwiftUI
+
+struct EmptyCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyCellView()
+}
---
+++ Pharmacin2.0/Others/ShowToast/ToastManager.swift
@@ -0,0 +1,8 @@
+//
+//  ToastManager.swift
+//  Pharmacin2.0
+//
+//  Created by Devin Maleke on 20/07/24.
+//
+
+import Foundation
